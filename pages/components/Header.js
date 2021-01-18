b604a0ad3e02de669082d4a356b964a50df8882e
Timothy Snyder <47593355+TimJSnyder@users.noreply.github.com>
2021-01-17 23:30:39 -0800

    Update Header.js

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,5 +1,7 @@
 import Appbar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function Header() {
     return (
@@ -8,6 +10,9 @@ export default function Header() {
         <h1 className="header">Notes</h1>
       </Appbar>
       <Toolbar />
+        <IconButton aria-label="github" color="secondary" href="https://github.com/TimJSnyder/Notes">
+          <GitHubIcon />
+        </IconButton>
       </>
     );
   }
